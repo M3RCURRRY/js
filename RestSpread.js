c25f92d17eb178129f79cd74ca0e950848e0a180
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-03-14 20:40:03 +0300

    Fix some prikol

--- RestSpread.js
+++ RestSpread.js
@@ -86,7 +86,7 @@ const { b1, ...brest } = array1;
 console.log(b1); // undefined
 console.log(brest); // object (0 : Array[ ['first', 1], ..., ['third', 3] ])
 
-const [o1, o2, o3] = { ...obj2 };
+const [o1, o2, o3] = obj2;
 console.log(`o1 : ${o1}, o2 : ${o2}, o3 : ${o3}`); // *pairs of entries*;
 
 const [a1, a2, a3] = array1;
