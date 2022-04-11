f08c44dc6f0517f45e85752210ee0ef24d6914be
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-04-11 10:55:03 +0300

    Create promise.js

---
+++ learnjs/promise.js
@@ -0,0 +1,11 @@
+console.clear();
+
+function delay(ms) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+delay(3000).then(() => console.log("выполнилось через 3 секунды"));
