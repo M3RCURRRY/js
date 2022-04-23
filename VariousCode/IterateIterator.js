d158ccd5cc5c7a0cd0ac5e18b89699cd2f407f7b
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-04-23 23:09:40 +0300

    Update IterateIterator.js

--- VariousCode/IterateIterator.js
+++ VariousCode/IterateIterator.js
@@ -1,4 +1,4 @@
-// HOW TO проитерироваться по итератору
+//HOW TO Перебрать генератор
 
 function* iterGen() {
   yield "a";
@@ -20,3 +20,20 @@ console.log(g1); // ["a", "b", "c"]
 
 const g2 = Array.from(iterGen());
 console.log(g2); // ["a", "b", "c"]
+
+//
+// Additional approach
+//
+
+const g3 = iterGen();
+let uselessFlag = false;
+
+for (let item of g3) {
+  if (uselessFlag) {
+    break;
+  }
+  console.log(`In for loop : ${item}`);
+  uselessFlag = true;
+}
+
+console.log(g3.next()); // value : undefined, done : true
