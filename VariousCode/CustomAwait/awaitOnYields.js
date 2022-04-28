fff939cc98350ccddea40df13441dbb134eca5f2
M3RCURRRY <m3rcurrry1337@gmail.com>
2022-04-29 00:05:39 +0300

    Update awaitOnYields.js

--- VariousCode/CustomAwait/awaitOnYields.js
+++ VariousCode/CustomAwait/awaitOnYields.js
@@ -1,21 +1,15 @@
 export default function awaitOnYields(generatorToProcess) {
-  let awaitResults = [];
-  let promiseValue = generatorToProcess.next();
-  console.log(promiseValue);
-  console.log(promiseValue.value);
-  while (promiseValue.done === false) {
-    console.log(promiseValue);
-    new Promise((resolve, reject) => {
-      resolve(
-        promiseValue.value.then((result) => {
-          return result;
-        })
-      );
-    }).then((r) => {
-			awaitResults.push(r);
-      promiseValue = generatorToProcess.next();
-    });
+  function callNext() {
+    let v = generatorToProcess.next();
+    if (v.done === false) {
+      v.value.then((res) => {
+        awaitResults.push(res);
+        callNext();
+      });
+    }
   }
-  console.log(promiseValue);
+  let awaitResults = [];
+  callNext();
+
   return awaitResults;
 }
